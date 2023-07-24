358d222c20411ef171433947c2f1a7e7586ba4ec
AnoopMasterCoder <anoop.github@gmail.com>
2023-07-24 20:24:54 +0530

    second commit

--- src/components/AboutMenu/AboutMenu.tsx
+++ src/components/AboutMenu/AboutMenu.tsx
@@ -35,7 +35,7 @@ const AboutMenu = () => {
                   ns='about'
                   components={[
                     <a
-                      href='https://github.com/ztjhz/BetterChatGPT'
+                      href='https://github.com/AnoopMasterCoder/BetterChatGPT'
                       target='_blank'
                       className='link'
                     />,
